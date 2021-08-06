c50d1d0d5a445f984312d44eaa204897d1831334
Sushanata Gupta <sushantagupta007.gmail.com>
2021-08-06 20:04:57 +0600

    solved code

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-// let createdArray = oddArrray();
-let createdArray = [1,3,5,3535,6634,46563,23232,465656555,43434,656528,438.083,3882937,109997883,2656,73838388383838,980895];
-function maxNumber(numbers){
-    let largest = 0; 
-    for(let i=0;i<numbers.length;i++){
-        if(numbers[i]>largest){
-            largest = numbers[i];
-        }
+// Sum of All Numers in an Array
+
+let myArray = [1,2,3,4,5,5,6,7,9,10,1.5,-1.5]
+
+function sumArray (numbers){
+    let total= 0;
+    for(let x=0; x<numbers.length;x++){
+        total+=numbers[x];
     }
-    return largest;
+    console.log(total);
 }
 
-let maximumNumber = maxNumber(createdArray);
-console.log(maximumNumber);
-
-function smallestNumber(numbers){
-    let smallest= maximumNumber;
-    for(let i=0;i<numbers.length;i++){
-        if(numbers[i]<smallest){
-            smallest = numbers[i];
-        }
-    }
-    console.log(smallest);
-}
-
-smallestNumber(createdArray);
-
+sumArray(myArray);
