74ac66ab1502d2af9f4c19b6f36ec63a35df9c66
Sushanata Gupta <sushantagupta007.gmail.com>
2021-08-06 22:32:38 +0600

    Factorial Code With NOwn Notes

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-/*function fibonaciGenarate(x){
-    let lastPrevious = 0;
-    let currentPrevious =1;
-    let sum; 
-    let fiboArray = [];
-    for(i=1; i<=x;i++){
-        sum=lastPrevious+currentPrevious;
-        [currentPrevious, lastPrevious]= [sum,currentPrevious]
-        fiboArray.push(sum);
-    }
-    console.log(fiboArray);
-}
-*/
-// fibonaciGenarate(10);
+// Higher to lower order
 
-// 
-//
-// fibo[2]=10;
-// fibo[3]= 12;
-// console.log(fibo);
-const fibo = [0,1];
-for(x=2; x<10;x++){
-    fibo[x]= fibo[x-1]+fibo[x-2];
+function factorialN(x){
+    let multiplication= 1;
+    for(i=x;i>0;i--){
+        multiplication=multiplication*i;
+    }
+    return multiplication;
 }
-console.log(fibo);
+
+let factorialNumber= factorialN(4);
+console.log(factorialNumber);
+
+
+//In for loop/while, the second criteria is a condition. It means there should somethin
+//like greater than and less than. For a detrimental loop, it should be >0. 
