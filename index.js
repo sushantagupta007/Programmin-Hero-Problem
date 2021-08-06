b56ca1a8c93f6ae37da3adc638fe537bd4442eb5
Sushanata Gupta <sushantagupta007.gmail.com>
2021-08-06 20:31:21 +0600

    positive and negative max and min value

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // let createdArray = oddArrray();
+//Maximum Number
 let createdArray = [1,3,5,3535,6634,46563,23232,465656555,43434,656528,438.083,3882937,109997883,2656,73838388383838,980895];
 function maxNumber(numbers){
-    let largest = 0; 
+    let largest = numbers[0]; 
     for(let i=0;i<numbers.length;i++){
         if(numbers[i]>largest){
             largest = numbers[i];
@@ -13,8 +14,9 @@ function maxNumber(numbers){
 let maximumNumber = maxNumber(createdArray);
 console.log(maximumNumber);
 
+//Smalles Number
 function smallestNumber(numbers){
-    let smallest= maximumNumber;
+    let smallest= numbers[0];
     for(let i=0;i<numbers.length;i++){
         if(numbers[i]<smallest){
             smallest = numbers[i];
@@ -25,3 +27,34 @@ function smallestNumber(numbers){
 
 smallestNumber(createdArray);
 
+
+let negativeArray = [-1,-4,-3,-0.3, -99];
+
+//Negative Smallest Number
+function negativeMaxValue (numbers){
+    let largest = numbers[0];
+    for(x=1;x<numbers.length;x++){
+        if(numbers[x]>largest){
+            largest = numbers[x];
+        }
+    }
+    console.log(largest);
+    
+}
+
+
+negativeMaxValue(negativeArray);
+
+//Negatvie Largets Number
+function negativeMinValue (numbers){
+    let smallest = numbers[0];
+    for(x=1;x<numbers.length;x++){
+        if(numbers[x]<smallest){
+            smallest = numbers[x];
+        }
+    }
+    console.log(smallest);
+    
+}
+
+negativeMinValue(negativeArray);
