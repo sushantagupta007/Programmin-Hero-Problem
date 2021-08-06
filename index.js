879c9ad8add932533e7759b183ecb2552eaf08bf
Sushanata Gupta <sushantagupta007.gmail.com>
2021-08-06 21:15:43 +0600

    code added

--- index.js
+++ index.js
@@ -1,60 +1,25 @@
-// let createdArray = oddArrray();
-//Maximum Number
-let createdArray = [1,3,5,3535,6634,46563,23232,465656555,43434,656528,438.083,3882937,109997883,2656,73838388383838,980895];
-function maxNumber(numbers){
-    let largest = numbers[0]; 
-    for(let i=0;i<numbers.length;i++){
-        if(numbers[i]>largest){
-            largest = numbers[i];
-        }
+/*function fibonaciGenarate(x){
+    let lastPrevious = 0;
+    let currentPrevious =1;
+    let sum; 
+    let fiboArray = [];
+    for(i=1; i<=x;i++){
+        sum=lastPrevious+currentPrevious;
+        [currentPrevious, lastPrevious]= [sum,currentPrevious]
+        fiboArray.push(sum);
     }
-    return largest;
+    console.log(fiboArray);
 }
+*/
+// fibonaciGenarate(10);
 
-let maximumNumber = maxNumber(createdArray);
-console.log(maximumNumber);
-
-//Smallest Number
-function smallestNumber(numbers){
-    let smallest= numbers[0];
-    for(let i=0;i<numbers.length;i++){
-        if(numbers[i]<smallest){
-            smallest = numbers[i];
-        }
-    }
-    console.log(smallest);
+// 
+//
+// fibo[2]=10;
+// fibo[3]= 12;
+// console.log(fibo);
+const fibo = [0,1];
+for(x=2; x<10;x++){
+    fibo[x]= fibo[x-1]+fibo[x-2];
 }
-
-smallestNumber(createdArray);
-
-
-let negativeArray = [-1,-4,-3,-0.3, -99];
-
-//Negative Largest Number
-function negativeMaxValue (numbers){
-    let largest = numbers[0];
-    for(x=1;x<numbers.length;x++){
-        if(numbers[x]>largest){
-            largest = numbers[x];
-        }
-    }
-    console.log(largest);
-    
-}
-
-
-negativeMaxValue(negativeArray);
-
-//Negatvie smallest Number
-function negativeMinValue (numbers){
-    let smallest = numbers[0];
-    for(x=1;x<numbers.length;x++){
-        if(numbers[x]<smallest){
-            smallest = numbers[x];
-        }
-    }
-    console.log(smallest);
-    
-}
-
-negativeMinValue(negativeArray);
+console.log(fibo);
