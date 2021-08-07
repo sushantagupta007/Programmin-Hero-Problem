1b42700e75dc0f5698321238a0c6a69f22ccd2eb
Sushanata Gupta <sushantagupta007.gmail.com>
2021-08-07 21:22:03 +0600

    Total Price Code

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-let newArray = [1,2,3,3,4,5,5];
+const phones  = [
+   {name:"Samsung", price:400,camera:10,storage:32, quantity:5}, 
+   {name:"walton32",price:400,camera:8,storage:32, quantity:5}, 
+   {name:"Shaomi32",price:400,camera:7,storage:32, quantity:5}, 
+   {name:"Shaomi32",price:400,camera:7,storage:32, quantity:5}, 
+   {name:"Shaomi32",price:400,camera:7,storage:32, quantity:5}, 
+   {name:"Shaomi32",price:400,camera:7,storage:32, quantity:5}, 
 
-let unique = [];
-
-//Direct Function 
-function removeDuplicate (names){
-for(i=0;i<names.length;i++){
-    const element = newArray[i];
-    if(unique.indexOf(element)==-1){
-        unique.push(element);
+]
+//NOrmal Loop
+/* function totalPriceForLoop (arrays){
+        let totalCost = 0;
+    for(let x=0; x<arrays.length;x++){
+        let mobilePrice = arrays[x].price;
+        totalCost+=mobilePrice;
     }
+    console.log(totalCost);
 }
-    return unique;
-}
-
-let newUnique = removeDuplicate(newArray);
-console.log(newUnique);
-
+totalPriceForLoop(phones); */
 
 //For of loop
 
-let newArray2 = [1,2,3,3,4,5,5,6,6,9,0,9];
+function totalPriceForOfLoop (arrays){
+    let total =0 ;
+    for(const element of arrays){
+        let quantity = element.quantity;
+        let mobileName = element.name;
+        let mobilePrice = element.price;
+        let totalPrice = quantity*mobilePrice;
+        console.log(mobileName, ":", mobilePrice, "bought", quantity);
+        total +=totalPrice;
+    }
+    console.log("Total Price",total);
+}
 
-function removeDuplicate (names){
-    let unique2 = [];
-    for (const element of names){
-        if(unique2.indexOf(element)==-1){
-            unique2.push(element);
-        }
-    }
-        return unique2;
-    }
-    
-    let newUnique2 = removeDuplicate(newArray2);
-    console.log(newUnique2);
-    
+totalPriceForOfLoop(phones)
